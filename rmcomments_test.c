808dcf56037d96841fb2b11163e32af5e4994704
Ulisse Mini <ulisse.mini@gmail.com>
2022-04-26 15:00:02 -0400

    Add terrible comment remover program

---
+++ rmcomments_test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* // Nested comment */
+
+int main(/* comment in args */) { // end of line comment
+  printf("Hello // comment inside string!\n");
+  printf("Hello /* comment inside string! */\n");
+  printf("some math: %f\n", 3.14 / 5.2 * 1.3)
+  // TODO: char comments?
+}
