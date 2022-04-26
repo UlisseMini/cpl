d94c10c51bfb7b87e5ff94f4ce19d7ec4908ebb8
Ulisse Mini <ulisse.mini@gmail.com>
2022-04-26 15:41:15 -0400

    Better design

--- rmcomments.c
+++ rmcomments.c
@@ -1,53 +1,60 @@
 #include <stdio.h>
 
-#define NORMAL 0
-#define STRING 1
-#define EOLCOMMENT 2
-#define INLINECOMMENT 3
 
-int main() {
-  char curr, peek;
-  int state = NORMAL;
-
-  // foo /* bar */
-  //
-
-  for (curr = getchar(); (peek = getchar()), curr != EOF; curr=peek) {
-    switch (state) {
-      case NORMAL:
-        if (curr == '"') {
-          state = STRING;
-          putchar(curr);
-        } else if (curr == '/' && peek == '/') {
-          state = EOLCOMMENT;
-        } else if (curr == '/' && peek == '*') {
-          state = INLINECOMMENT;
-        } else {
-          putchar(curr);
-        }
-        break;
-      case STRING:
-        if (curr == '"') {
-          state = NORMAL;
-        }
-        putchar(curr);
-        break;
-      case EOLCOMMENT:
-        if (curr == '\n') {
-          state = NORMAL;
-          putchar(curr);
-        }
-        break;
-      case INLINECOMMENT:
-        if (curr == '*' && peek == '/') {
-          state = NORMAL;
-          // Advance
-          curr = peek;
-          peek = getchar();
-        }
-        break;
+void string() {
+  char c;
+  while ((c = getchar()) != EOF) {
+    putchar(c);
+    if (c != '\\') {
+      c = getchar();
+      putchar(c);
+      if (c == '"') break;
     }
   }
+}
+
+void inlinecomment() {
+  char c;
+  while ((c = getchar()) != EOF) {
+    if (c == '*' && (getchar() == '/'))
+      break;
+  }
+}
+
+void linecomment() {
+  char c;
+  while ((c = getchar()) != EOF) {
+    if (c == '\n')
+      break;
+  }
+  putchar(c);
+}
+
+
+void normal() {
+  char c;
+  while ((c = getchar()) != EOF) {
+    if (c == '"') {
+      putchar(c);
+      string();
+    } else if (c == '/') {
+      char peek = getchar();
+      if (peek == '/') {
+        linecomment();
+      } else if (peek == '*') {
+        inlinecomment();
+      } else {
+        putchar(c);
+        putchar(peek);
+      }
+    } else {
+      putchar(c);
+    }
+  }
+}
+
+int main() {
+  normal();
 
   return 0;
 }
--- rmcomments_test.c
+++ rmcomments_test.c
@@ -2,9 +2,12 @@
 
 /* // Nested comment */
 
+/* a comment *//* next to another */
+
 int main(/* comment in args */) { // end of line comment
   printf("Hello // comment inside string!\n");
   printf("Hello /* comment inside string! */\n");
-  printf("some math: %f\n", 3.14 / 5.2 * 1.3)
+  printf("some math: %f\n", 3.14 / 5.2 /*foo*/* 1.3);
+  printf("escaped \"\n");
   // TODO: char comments?
 }
